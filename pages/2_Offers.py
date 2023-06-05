f81acc5b790832682259f1d59cd37ed0a8ac0cfc
clothme <57736523+clothme-io@users.noreply.github.com>
2023-06-05 12:25:31 -0700

    added requirement.txt

--- pages/2_Offers.py
+++ pages/2_Offers.py
@@ -1,5 +1,7 @@
 import datetime
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 import pickle
 
 # import matplotlib.pyplot as plt
